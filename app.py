37dda4a1372a256cc52d28f597f854ff402f0a8f
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-06-14 18:21:41 -0300

    feat: adiciona lógica de remoção de veículos com validação de vagas

--- app.py
+++ app.py
@@ -42,5 +42,30 @@ def calcular_potencia_por_carro(qnt_carros_no_posto):
         potencia_por_carro = potencia_max_posto / qnt_carros_no_posto
 
     return potencia_por_carro
-    
 
+def remover_carro():
+    global carros_no_posto
+    global qnt_carros_no_posto
+    
+    if not carros_no_posto:
+        print("O posto já está vazio!")
+        return
+    
+    for indice, carro in enumerate(carros_no_posto):
+        print(f'Vaga {indice + 1} -- {carro}')
+    print('-' * 20)
+    
+    selecionar_veiculo = False
+    while not selecionar_veiculo:
+        try:
+            vaga_escolhida = int(input('Digita a vaga do carro a sair: '))
+            if 1 <= vaga_escolhida <= len(carros_no_posto):
+                indice_remover = vaga_escolhida - 1
+                carros_no_posto.pop(indice_remover)
+                qnt_carros_no_posto -= 1
+                selecionar_veiculo = True
+            else:
+                print('Vaga inválida! Selecione uma vaga ocupada da lista.')
+        except ValueError:
+            print('Por favor, digite um número válido.')
+    print('Carro removido.')
